95e9f6435fc4411c68c0c9765becfa72536738f3
samat <e107ti@etec.sp.gov.br>
2024-04-18 19:53:20 -0300

    nome em js

---
+++ script.js
@@ -0,0 +1,9 @@
+alert ("Olá user, tudo bem?");
+
+let nome = prompt("Poderia me informar seu primeiro nome?");
+
+let sobrenome = prompt("Poderia me informar seu sobrenome?");
+
+alert("Seu nome é: " + nome + " " + sobrenome);
+
+alert("Seu nome de catálogo: " + sobrenome.toUpperCase() + " , " + nome);
